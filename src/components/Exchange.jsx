33a3951e93e6920836ee27ef5a4f87b86a548444
AbdullahMazher1 <abd.sialvi64@gmail.com>
2026-02-10 20:45:26 +0500

    svg folder

--- src/components/Exchange.jsx
+++ src/components/Exchange.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {gimIcon} from '../assets/Svg'
 
 function Exchange() {
   return (
-    <div className=''>
-      
+    <div className='flex flex-col gap-12 bg-black'>
+        <div className=''>
+            <div className='flex flex-row'>
+                <div>
+                    <gimIcon/>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
