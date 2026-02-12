55cec271723d489dae96173928dfb0ec90410cf2
AbdullahMazher1 <abd.sialvi64@gmail.com>
2026-02-12 19:01:51 +0500

    a

--- src/components/FutuuraMatters.jsx
+++ src/components/FutuuraMatters.jsx
@@ -26,23 +26,23 @@ function FutuuraMatters() {
         <source src="/Videos/Futurua.mp4" type="video/mp4" />
       </video>
 
-            {/* 2. The Center Notch/Hole (Semi-Circle Style) */}
-            <div className="absolute top-0 left-1/2 -translate-x-1/2 z-30">
-                <div className="relative w-[200px] h-[100px] md:w-[320px] md:h-[160px] flex items-center justify-center">
-                    {/* The Cutout Shape: rounded-b-full creates the semi-circle effect */}
-                    <div className="absolute top-0 w-full h-full bg-black rounded-b-full border-x border-b border-white/10" />
+      {/* 2. The Center Notch/Hole (Semi-Circle Style) */}
+      <div className="absolute top-0 left-1/2 -translate-x-1/2 z-30">
+        <div className="relative w-[200px] h-[100px] md:w-[320px] md:h-[160px] flex items-center justify-center">
+          {/* The Cutout Shape: rounded-b-full creates the semi-circle effect */}
+          <div className="absolute top-0 w-full h-full bg-black rounded-b-full border-x border-b border-white/10" />
 
-                    {/* Floating Image */}
-                    <div className="relative z-10 w-full h-full mt-[-10%]">
-                        <Image
-                            src="/Images/Rectangle.png"
-                            alt="Futuura Core"
-                            fill
-                            className="object-contain"
-                        />
-                    </div>
-                </div>
-            </div>
+          {/* Floating Image */}
+          <div className="relative z-10 w-full h-full mt-[-10%]">
+            <Image
+              src="/Images/Rectangle.png"
+              alt="Futuura Core"
+              fill
+              className="object-contain"
+            />
+          </div>
+        </div>
+      </div>
 
       {/* Main Content */}
       <div className="relative z-20 w-full max-w-[1400px] mt-16 sm:mt-20">
@@ -52,7 +52,7 @@ function FutuuraMatters() {
           {/* Left Text */}
           <div className="lg:col-span-5 flex flex-col gap-4 text-center lg:text-left">
             <div className="flex items-center justify-center lg:justify-start gap-2">
-              <GimIcon/>
+              <GimIcon />
               <span className="text-white text-xs tracking-widest">
                 Fuutura Matters in Pakistan
               </span>
@@ -80,13 +80,13 @@ function FutuuraMatters() {
 
               {/* Scrolling List */}
               <div className="relative overflow-hidden h-[220px] sm:h-[260px] md:h-[300px]">
-                <div className="absolute w-full flex flex-col gap-8 p-6 animate-slow-scroll">
+                <div className="absolute w-full flex flex-col gap-12 p-6 animate-slow-scroll">
                   {[...scrollingData, ...scrollingData].map((item, idx) => (
                     <div key={idx} className="flex items-center gap-3">
                       <div className="relative w-8 h-8 sm:w-10 sm:h-10">
-                        <Image src={item.icon} alt={item.title} fill className="object-contain" />
+                        <Image src={item.icon} alt={item.title} fill className="object-contain h-full w-full" />
                       </div>
-                      <span className="text-white text-sm sm:text-base font-medium">
+                      <span className="text-white text-[20px] sm:text-base font-medium">
                         {item.title}
                       </span>
                     </div>
@@ -94,15 +94,15 @@ function FutuuraMatters() {
                 </div>
               </div>
 
-                            {/* GIF / Empty Space Side */}
-                            <div className="">
+              {/* GIF / Empty Space Side */}
+              <div className="">
 
 
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
 
       {/* Scroll Animation */}
       <style jsx>{`
